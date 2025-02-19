439bc10ad65d70ddd08bff0f9264f0c2330d66d5
Marina <marina.philogene@gmail.com>
2025-02-19 18:38:39 +0100

    test css 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Ajoutez ici des configurations webpack personnalisées si nécessaire
+    return config;
+  },
+  // Ajoutez cette ligne si vous utilisez des modules CSS
+  cssModules: true,
+  // Désactivez temporairement ESLint pendant le build si nécessaire
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
